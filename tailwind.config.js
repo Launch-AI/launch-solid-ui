5d0b2a84083009e0ee987a5e5f04d85a8a2f4b88
Ari Seyhun <ariseyhun@live.com.au>
2021-06-30 20:44:28 +0930

    feat: add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,10 @@ module.exports = {
       borderRadius: {
         '3xl': '1.5rem',
       },
+      fontFamily: {
+        sans:
+          '"Inter",ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+      },
       fontSize: {
         base: '0.9375rem',
       },
