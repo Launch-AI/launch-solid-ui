cba7a68d5d45cd9ddedc0c7d24786c285dbe0697
zedaan <zedanrumi@gmail.com>
2021-07-01 12:14:29 +0500

    feat: removed any keyword

--- src/components/radio/StyledRadio.ts
+++ src/components/radio/StyledRadio.ts
@@ -3,7 +3,7 @@ import tw from 'twin.macro'
 
 import { IRadioProps } from './Radio'
 
-const StyledLabel: any = tw`relative flex items-center cursor-pointer`
+const StyledLabel = tw`relative flex items-center cursor-pointer`
 
 export default styled('label')<IRadioProps & JSX.IntrinsicElements['label']>(
   StyledLabel
