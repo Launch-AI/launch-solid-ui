be0e9562d87235fc67ee398f6f2e4f4dfda1cdf0
Ari Seyhun <ariseyhun@live.com.au>
2021-08-03 21:31:55 +0930

    feat: add block true to storybook

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -5,31 +5,35 @@ const InputTemplate: Story = (args) => <Input {...args} />
 export const Default = InputTemplate.bind({})
 Default.args = {
   placeholder: 'Placeholder',
+  block: false,
 }
 
 export const Filled = InputTemplate.bind({})
 Filled.args = {
   placeholder: 'Placeholder',
   filled: true,
+  block: false,
 }
 
 export const WithLabel = InputTemplate.bind({})
 WithLabel.args = {
   placeholder: 'Placeholder',
   label: 'Label',
+  block: false,
 }
 
 export const WithFloatLabel = InputTemplate.bind({})
 WithFloatLabel.args = {
   label: 'Label',
   labelFloat: true,
+  block: false,
 }
 
 export const Block = InputTemplate.bind({})
 Block.args = {
   label: 'Label',
   labelFloat: true,
-  block: false,
+  block: true,
 }
 
 export default {
